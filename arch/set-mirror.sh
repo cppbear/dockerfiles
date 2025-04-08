4dae8a721242bf1b02386f51a2d44247579e1d62
cppbear <chubei99@outlook.com>
2025-04-08 13:28:50 +0800

    Refactor set-mirror.sh for improved readability
    Signed-off-by: cppbear <chubei99@outlook.com>

--- arch/set-mirror.sh
+++ arch/set-mirror.sh
@@ -7,10 +7,10 @@ if [[ $UID -ne 0 ]]; then
     exit 1
 fi
 
-mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak && \
-&& echo 'Server = https://mirror.nju.edu.cn/archlinux/$repo/os/$arch' > /etc/pacman.d/mirrorlist \
-&& echo '[archlinuxcn]' >> /etc/pacman.conf \
-&& echo 'Server = https://mirror.nju.edu.cn/archlinuxcn/$arch' >> /etc/pacman.conf \
-&& pacman -Syu --noconfirm archlinuxcn-keyring && pacman-key --populate archlinuxcn \
-&& pacman -Syu --noconfirm --needed paru \
-&& sed -i 's/^#BottomUp/BottomUp/' /etc/paru.conf
+mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
+echo 'Server = https://mirror.nju.edu.cn/archlinux/$repo/os/$arch' > /etc/pacman.d/mirrorlist
+echo '[archlinuxcn]' >> /etc/pacman.conf
+echo 'Server = https://mirror.nju.edu.cn/archlinuxcn/$arch' >> /etc/pacman.conf
+pacman -Syu --noconfirm archlinuxcn-keyring && pacman-key --populate archlinuxcn
+pacman -Syu --noconfirm --needed paru
+sed -i 's/^#BottomUp/BottomUp/' /etc/paru.conf
